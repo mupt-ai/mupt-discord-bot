52c09219bea55b5614c41fc0d4609b90a162cf62
Benjamin Hong <byhong@caltech.edu>
2023-12-30 20:25:47 -0800

    bot template

--- src/kory.py
+++ src/kory.py
@@ -1,7 +1,7 @@
 from bot import MuptBot
 import os
 
-token = os.environ["MUPT_BOT_TOKEN"]
+token = os.environ["KORY_BOT_TOKEN"]
 key = os.environ["FIREWORKS_CLIENT_API_KEY"]
 bot = MuptBot(token, key)
 bot.run()
