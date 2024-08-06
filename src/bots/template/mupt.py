ecb8e2f05cf89b98d580db7facff0a94dba7a4bd
Benjamin Hong <byhong@caltech.edu>
2024-08-05 21:02:31 -0700

    updated bot template code

--- src/bots/template/mupt.py
+++ src/bots/template/mupt.py
@@ -1,16 +1,35 @@
 import os
 import sys
+from dotenv import load_dotenv
+from openai import OpenAI
 
 # Get the directory of the current script
 current_dir = os.path.dirname(os.path.abspath(__file__))
-# Get the src root directory (one level up)
-src_root = os.path.dirname(current_dir)
+# Get the src root directory (two levels up)
+src_root = os.path.dirname(os.path.dirname(current_dir))
 # Now can import bot.py using the absolute path
 sys.path.append(src_root)
 
-from bot import MuptBot
+from bot import MuptBot  # Import the MuptBot class from bot.py
+
+# Load environment variables
+load_dotenv()
+
+# OpenAI API key setup
+client = OpenAI(
+  api_key=os.environ.get("MUPT_OPENAI_API_KEY"),
+)
+
+###########################
+# BOT-SPECIFIC PARAMETERS #
+###########################
 
 token = os.environ["MUPT_BOT_TOKEN"]
-key = os.environ["FIREWORKS_CLIENT_API_KEY"]
-bot = MuptBot(token, key)
+context_length = os.environ["GENERAL_CONTEXT_LENGTH"]
+prompt = ""
+
+###########################
+
+# Bot setup and run
+bot = MuptBot(token, context_length, prompt, False)
 bot.run()
